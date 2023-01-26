4f3ed334459f548776bcb40f08167e3fc2091995
kchelle robinson <rkchelle64@gmail.com>
2023-01-26 11:04:26 -0600

    Create TrainSataion
    Adding TrainStation

---
+++ SP23.P01.Web/TrainStation.cs
@@ -0,0 +1,29 @@
+﻿namespace SP23.P01.Web
+{
+    public class TrainStation
+    {
+           public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Address { get; set; }
+
+
+        public class TrainStationDto
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public string Address { get; set; }
+
+        }
+
+        public class TrainStationGetDto
+        {
+            public int Id { get; set; }
+
+            public string Name { get; set; }
+            public int Address { get; set; }
+        }
+    }
+}
+}
