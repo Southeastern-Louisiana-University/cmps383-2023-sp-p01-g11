335e3894decb921c25cb6e73e3afb2dd2b115cf7
AutumnKennedy <autumnkennedy0714@gmail.com>
2023-01-30 14:34:37 -0600

    Added connection String

--- SP23.P01.Web/Program.cs
+++ SP23.P01.Web/Program.cs
@@ -1,8 +1,12 @@
+using Microsoft.EntityFrameworkCore;
 using SP23.P01.Web;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddDbContext<DataContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DataContext")));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
